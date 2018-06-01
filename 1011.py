8f5557d97065ff2270e821c01fa1c9d5a41adbdb
deltacluse <deltacluse@naver.com>
2018-06-01 10:30:32 +0900

    .
    1011

---
+++ 1011.py
@@ -0,0 +1,18 @@
+# 1011 Fly me to the Alpha Centauri
+
+
+def f_1011():
+    x = int(input())
+    y = int(input())
+    distance = x - y
+    
+
+def main():
+    T = int(input())
+
+    for i in range(3):
+        f_1011()
+
+
+if __name__ == "__main__":
+    main()
